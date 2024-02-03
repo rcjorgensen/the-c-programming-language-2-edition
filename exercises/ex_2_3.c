6ebc4e9e713062163b803508924af638e47b5eda
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2024-02-04 00:28:26 +0100

    Add solution

--- exercises/ex_2_3.c
+++ exercises/ex_2_3.c
@@ -2,7 +2,8 @@
 
 int main()
 {
-    int result = htoi("4f8ace01");
+    /* int result = htoi("0x4f8ace01"); */
+    int result = htoi("0xAa");
     printf("%d\n", result);
 }
 
@@ -11,14 +12,19 @@ int htoi(char s[])
 {
     int i, j, n;
 
+    i = 0;
     n = 0;
 
-    for (i = 0; s[i] >= '0' && s[i] <= '9' || s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z'; ++i) {
+    if (s[0] == '0' && (s[1] == 'x' || s[1] == 'X')) {
+        i = 2;
+    }
+
+    for (; s[i] >= '0' && s[i] <= '9' || s[i] >= 'a' && s[i] <= 'f' || s[i] >= 'A' && s[i] <= 'F'; ++i) {
         if (s[i] >= '0' && s[i] <= '9') {
             j = s[i] - '0';
-        } else if (s[i] >= 'a' && s[i] <= 'z') {
+        } else if (s[i] >= 'a' && s[i] <= 'f') {
             j = s[i] - 'a' + 10;
-        } else if (s[i] >= 'A' && s[i] <= 'Z') {
+        } else if (s[i] >= 'A' && s[i] <= 'F') {
             j = s[i] - 'A' + 10;
         }
 
